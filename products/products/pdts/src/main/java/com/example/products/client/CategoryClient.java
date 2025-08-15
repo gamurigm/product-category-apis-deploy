9a7fe80cf24dca9d7be035dee8c3e2b9f5448f80
Gabriel Murillo Medina <gamurigm@gmail.com>
2025-08-15 15:06:35 -0500

    corregido end point nube...

--- products/products/pdts/src/main/java/com/example/products/client/CategoryClient.java
+++ products/products/pdts/src/main/java/com/example/products/client/CategoryClient.java
@@ -7,9 +7,9 @@ import java.util.List;
 
 @FeignClient(name = "category-service", url = "${CATEGORIES_SERVICE_URL:http://localhost:8089}")
 public interface CategoryClient {
-    @GetMapping("/api/categorias")
+    @GetMapping("")
     List<CategoryDTO> getAllCategories();
 
-    @GetMapping("/api/categorias/{id}")
+    @GetMapping("/{id}")
     CategoryDTO getCategoryById(@PathVariable("id") Long id);
 }
